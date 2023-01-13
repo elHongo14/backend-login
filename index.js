9ef9e8d34629b23c6f1b0af72c12b105d6f641ef
elHongo14 <elhongo1409@outlook.com>
2023-01-13 15:07:31 -0600

    Xd

--- index.js
+++ index.js
@@ -12,10 +12,19 @@ app.use(bodyparser.urlencoded({
 app.use(bodyparser.json())
 
 // Conexión a BD
+const url = `mongodb+srv://esotilin:${process.env.PASSWORD}@cluster0.vlsp8xo.mongodb.net/${process.env.DB}`
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('conectao a bd XDXFXDCXDXCHJMANAJASG'))
+    .catch((error) => console.log('error de bd, tas todo wey: ' + error))
 
 // Importar las Rutas
+const authRoutes = require('./routes/auth')
 
 // Ruta para el Middleware
+app.use('/api/user', authRoutes)
+
 app.get('/', (req, res) => {
     res.json({
         estado: true,
